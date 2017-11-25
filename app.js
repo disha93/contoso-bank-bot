79c08ab7aff81916474894ae5871e0e366a6a2c2
disha93 <shomaldass@hotmail.com>
2017-11-25 15:55:25 +1300

    Added project files

---
+++ app.js
@@ -0,0 +1,26 @@
+// Author: Shomal Dass
+
+var restify = require('restify');
+var botBuilder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Creates a chat connector for communicating with the Bot Framework Service
+var connector = new botBuilder.ChatConnector({
+    //Add MS credentials
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond to each message appropriately.
+var bot = new botBuilder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
+luis.startDialog(bot);
